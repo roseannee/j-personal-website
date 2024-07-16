0407cb3b42c8b99dd8686b9290b24243abf506e3
Daria <roseannee.kim@gmail.com>
2024-07-16 13:25:55 +0200

    refactor: update site.ts

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Template",
-  description: "Starter template.",
+  name: "Website",
+  description: "J Personal Website",
   mainNav: [
     {
       title: "Home",
