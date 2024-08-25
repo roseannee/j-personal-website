af62e4ef06de329d7f00e94d324435a675925310
Daria <roseannee.kim@gmail.com>
2024-08-25 18:59:56 +0200

    refactor: update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,11 @@ const nextConfig = {
       },
     ],
   },
+  outputFileTracingIncludes: {
+    "/sign-in": [
+      `./node_modules/argon2/prebuilds/${process.env.ARGON2_PREBUILDS_GLOB || "**"}`,
+    ],
+  },
 }
 
 export default nextConfig
